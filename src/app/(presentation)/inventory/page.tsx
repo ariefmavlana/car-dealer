11b08ae971651010a212f46f33f6e044ffcccfa2
ariefmavlana <ariefmaulana0808@gmail.com>
2025-06-02 13:24:12 +0700

    comm 2, layouts settings

---
+++ src/app/(presentation)/inventory/page.tsx
@@ -0,0 +1,190 @@
+import { ClassifiedList } from "@/components/inventory/classified-list"
+import { DialogFilters } from "@/components/inventory/dialog-filters"
+import { Sidebar } from "@/components/inventory/sidebar"
+import { CustomPagination } from "@/components/shared/custom-pagination"
+import { CLASSFIEDS_PER_PAGE } from "@/config/constants"
+import { routes } from "@/config/routes"
+import { AwaitedPageProps, Favourites, PageProps } from "@/config/types"
+import { prisma } from "@/lib/prisma"
+import { redis } from "@/lib/redis-store"
+import { getSourceId } from "@/lib/source-id"
+import { ClassifiedStatus, Prisma } from "@prisma/client"
+import { parseAsString } from "nuqs"
+import { z } from "zod"
+
+const PageSchema = z.string().transform((val) => Math.max(Number(val), 1)).optional()
+
+const ClassifiedFilterSchema = z.object({
+  q: z.string().optional(),
+    make: z.string().optional(),
+    model: z.string().optional(),
+    modelVariant: z.string().optional(),
+    minYear: z.string().optional(),
+    maxYear: z.string().optional(),
+    minPrice: z.string().optional(),
+    maxPrice: z.string().optional(),
+    minReading: z.string().optional(),
+    maxReading: z.string().optional(),
+    currency: z.string().optional(),
+    odoUnit: z.string().optional(),
+    transmission: z.string().optional(),
+    fuelType: z.string().optional(),
+    bodyType: z.string().optional(),
+    colour: z.string().optional(),
+    doors: z.string().optional(),
+    seats: z.string().optional(),
+    ulezCompliance: z.string().optional(),
+})
+
+const buildClassifiedFilterQuery = (
+  searchParams: AwaitedPageProps["searchParams"] | undefined,
+): Prisma.ClassifiedWhereInput => {
+  const { data } = ClassifiedFilterSchema.safeParse(searchParams)
+
+  if (!data) return { status: ClassifiedStatus.LIVE }
+
+  const keys = Object.keys(data)
+
+  const taxonomyFilters = ["make", "model", "modelVariant"]
+
+  const rangeFilters = {
+    minYear: "year",
+    maxYear: "year",
+    minPrice: "price",
+    maxPrice: "price",
+    minReading: "odoReading",
+    maxReading: "odoReading",
+  }
+
+  const numFilters = ["seats", "doors"]
+  const enumFilters = ["odoUnit", "currency", "transmission", "bodyType", "fuelType", "colour", "ulezCompliant"]
+
+  const mapParamsToFields = keys.reduce((acc, key) => {
+    const value = searchParams?.[key] as string | undefined
+    if (!value) return acc
+
+    if (taxonomyFilters.includes(key)) {
+      acc[key] = { id: Number(value) }
+    } else if (enumFilters.includes(key)) {
+      acc[key] = value.toUpperCase()
+    } else if (numFilters.includes(key)) {
+      acc[key] = Number(value)
+    } else if (key in rangeFilters) {
+      const field = rangeFilters[key as keyof typeof rangeFilters]
+      acc[field] = acc[field] || {}
+      if (key.startsWith("min")) {
+        acc[field].get = Number(value)
+      } else if (key.startsWith('max')) {
+        acc[field].let = Number(value)
+      }
+    }
+    return acc
+  }, {} as {[key: string]: any},
+)
+
+  return {
+    status: ClassifiedStatus.LIVE,
+
+    ...(searchParams?.q && {
+      OR: [
+        {
+          title: {
+            contains: searchParams.q as string,
+            mode: "insensitive"
+          }
+        },
+
+        {
+          description: {
+            contains: searchParams.q as string,
+            mode: "insensitive"
+          },
+        },
+      ],
+    }),
+
+    ...mapParamsToFields
+  }
+}
+
+const getInventory = async (searchParams: AwaitedPageProps['searchParams']) => {
+  const validPage = PageSchema.parse(searchParams?.page)
+
+  // get the current page
+	const page = validPage ? validPage : 1
+
+  // calculate the offset
+	const offset = (page - 1) * CLASSFIEDS_PER_PAGE
+
+  return prisma.classified.findMany({
+    where: buildClassifiedFilterQuery(searchParams),
+    include: { images: {take: 1} },
+    skip: offset,
+    take: CLASSFIEDS_PER_PAGE,
+  })
+}
+
+export default async function InventoryPage(props: PageProps) {
+  const searchParams = await props.searchParams
+  const classifieds = await getInventory(searchParams)
+  const count = await prisma.classified.count({
+    where: buildClassifiedFilterQuery(searchParams)
+  })
+
+  const minMaxResult = await prisma.classified.aggregate({
+    _min: {
+      year: true,
+      price: true,
+      odoReading: true,
+    },
+    _max: {
+      price: true,
+      year: true,
+      odoReading: true,
+    },
+  })
+
+  const sourceId = await getSourceId()
+  const favourites = await redis.get<Favourites>(sourceId ?? "")
+  const totalPages = Math.ceil(count / CLASSFIEDS_PER_PAGE)
+
+  return (
+    <div className="flex">
+      <Sidebar minMaxValues={null} searchParams={searchParams}/>
+
+      <div className="flex-1 p-4 bg-white">
+        <div className="flex space-y-2 flex-col lg:flex-row items-center justify-center pb-4 -mt-1">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="text-sm text-gray-600 md:text-base lg:text-xl font-semibold min-w-fit">
+              We have found {count} classifieds...
+            </h2>
+            <DialogFilters minMaxValues={minMaxResult} count={count} searchParams={searchParams}/>
+          </div>
+          <CustomPagination
+          baseURL={routes.inventory}
+          totalPages={totalPages}
+          styles={{
+            paginationRoot: "justify-end hidden lg:flex",
+            paginationPrevious: "",
+            paginationNext: "",
+            paginationLink: "border-none active:border text-black",
+            paginationLinkActive: ""
+          }}
+          />
+        </div>
+        <ClassifiedList classifieds={classifieds} favourites={favourites ? favourites.ids : []}/>
+        <CustomPagination
+          baseURL={routes.inventory}
+          totalPages={totalPages}
+          styles={{
+            paginationRoot: "justify-center lg:hidden pt-12",
+            paginationPrevious: "",
+            paginationNext: "",
+            paginationLink: "border-none active:border",
+            paginationLinkActive: ""
+          }}
+          />
+      </div>
+    </div>
+  )
+}
